f480d0473102ee0659f67aa0e4940c64239328cd
AShah326 <aryantx@outlook.com>
2025-12-06 22:20:21 -0600

    fix indentation for optimizer initialization in train function

--- scripts/swin_unetr_btcv_setup.py
+++ scripts/swin_unetr_btcv_setup.py
@@ -534,7 +534,7 @@ def train(
         softmax=True,
         include_background=True,
     )
-        optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-5)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-5)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max(1, epochs))
 
     for epoch in range(epochs):
